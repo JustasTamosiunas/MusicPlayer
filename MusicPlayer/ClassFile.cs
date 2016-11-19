f2f8f3db421f0245db08399feea070e0f599edc3
Justas Tamošiūnas <runjustis@gmail.com>
2016-11-19 14:53:40 +0200

    Begun adding playlist functionality

--- MusicPlayer/ClassFile.cs
+++ MusicPlayer/ClassFile.cs
@@ -5,13 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using NAudio;
 using NAudio.Wave;
+using System.IO;
 
 namespace MusicPlayer {
     class MusicPlayerClass 
     {
-        public MusicPlayerClass() {
-
-        }
+        public MusicPlayerClass() {}
         private IWavePlayer waveOutDevice = new WaveOut(); //We're using the NAudio library (included with the project), we create the player, and the file reader.
         private MediaFoundationReader mediaFoundationReader;
         public void changeMusic(string location) {
@@ -24,6 +23,34 @@ namespace MusicPlayer {
         }
         public void playMusic() {
             waveOutDevice.Play();
+        }
+    }
+
+    class Playlist
+    {
+        string name;
+        List<string> songList = new List<string>(); //strings with all song Paths
+        int position;
+        public string nextSong() {
+            if (!(position == songList.Count-1)) { //if it's not the last song, return the next one. If it is, loop around.
+                position++; 
+                return songList[position];
+            } else {
+                position = 0;
+                return songList[position];
+            }
+        }
+        public void initialize() {
+            using (StreamReader sr = new StreamReader("playlists.dat")) {
+                name = sr.ReadToEnd();
+                position = int.Parse(sr.ReadToEnd());
+                while(true) {
+                    string line = sr.ReadToEnd();
+                    if (line == "&&&")
+                        break;
+                    songList.Add(line);
+                }
+            }
         }
     }
 }
